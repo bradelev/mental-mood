6504aa40505efd116b0fece8a6f4670e53fc5890
Braulio De Leon <hi@brauliodeleon.com>
2024-09-25 22:46:16 -0300

    fix: eslint errors

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Stepper from '../components/Stepper';
+
+export default function Profile() {
+  return (
+    <div>
+      <Stepper currentStep={5} />
+      {/* Aquí va el contenido de las metas */}
+    </div>
+  );
+}
